9c2e64fe4b540cbe13ed482d5b8cba8d32fa06e4
Prelang Builder <builder@prelang.com>
2015-03-03 23:32:02 +0000

    Generated scaffolding for model 'Deck' via Rails

---
+++ app/views/decks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @deck, :id, :name, :styling, :fields, :created_at, :updated_at
---
+++ test/controllers/decks_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class DecksControllerTest < ActionController::TestCase
+  setup do
+    @deck = decks(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:decks)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create deck" do
+    assert_difference('Deck.count') do
+      post :create, deck: { fields: @deck.fields, name: @deck.name, styling: @deck.styling }
+    end
+
+    assert_redirected_to deck_path(assigns(:deck))
+  end
+
+  test "should show deck" do
+    get :show, id: @deck
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @deck
+    assert_response :success
+  end
+
+  test "should update deck" do
+    patch :update, id: @deck, deck: { fields: @deck.fields, name: @deck.name, styling: @deck.styling }
+    assert_redirected_to deck_path(assigns(:deck))
+  end
+
+  test "should destroy deck" do
+    assert_difference('Deck.count', -1) do
+      delete :destroy, id: @deck
+    end
+
+    assert_redirected_to decks_path
+  end
+end
